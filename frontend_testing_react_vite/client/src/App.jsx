3d897ba6d8116cf00dd0e8582781a909fc9e1d7e
JaGarcia02 <jagarcia02051998@gmail.com>
2023-07-13 16:24:09 +0800

    Dynamic add and remove

--- frontend_testing_react_vite/client/src/App.jsx
+++ frontend_testing_react_vite/client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Recipe_Page from "./pages/Recipe_Page";
+import DynamicAddRemove_Page from "./pages/DynamicAddRemove_Page";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/test" element={<Recipe_Page />} />
+        <Route path="/test2" element={<DynamicAddRemove_Page />} />
       </Routes>
     </>
   );
