a661580f1544dd717e586235ff0ba117e5214314
JaGarcia02 <jagarcia02051998@gmail.com>
2023-07-27 17:30:02 +0800

    test react-excel-renderer

--- frontend_testing_react_vite/react_excel/src/pages/ReactExcelRenderer_01.jsx
+++ frontend_testing_react_vite/react_excel/src/pages/ReactExcelRenderer_01.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { OutTable, ExcelRenderer } from "react-excel-renderer";
 import axios from "axios";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const ReactExcelRenderer_01 = () => {
   const [header, setHeader] = useState([]);
   const [cols, setCols] = useState([]);
   const [emp, setEmp] = useState([]);
+
+  useEffect(() => {
+    axios.get();
+  }, []);
 
   return <div>ReactExcelRenderer_01</div>;
 };
