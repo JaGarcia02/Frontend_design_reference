4a711e7d20a8b19efd8bcb06999e44eaf80e525f
Ja Garcia <jagarcia02051998@gmail.com>
2023-08-21 16:09:51 +0800

    update my project

--- MyPortfolioWebsite/frontend/src/App.jsx
+++ MyPortfolioWebsite/frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import Aos from "aos";
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
   return (
-    <div className="w-screen h-screen bg-red-300">
-      <h1>Hello</h1>
+    <div className="w-screen h-scree">
+      <h1 className="text-indigo-400 text-2xl">Hello</h1>
     </div>
   );
 }
