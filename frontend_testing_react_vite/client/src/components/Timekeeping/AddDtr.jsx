e2fab9627632b3a06902349a5684cfc3af476c61
JaGarcia02 <jagarcia02051998@gmail.com>
2023-09-11 15:37:02 +0800

    timekeeping prototype

---
+++ frontend_testing_react_vite/client/src/components/Timekeeping/AddDtr.jsx
@@ -0,0 +1,242 @@
+import React, { useState } from "react";
+import uniqid from "uniqid";
+
+const AddDtr = ({ setOpenAdd }) => {
+  const [dataArray, setDataArray] = useState({ data: [] });
+  const [dtrInput, setDtrInput] = useState({
+    id: "",
+    cutoff: "",
+    timeIn: "",
+    breakStart: "",
+    breakEnd: "",
+    timeOut: "",
+    bioId: "",
+  });
+
+  const SaveArray = () => {
+    if (!dtrInput) {
+      alert("empty");
+    } else {
+      setDataArray({ ...dataArray, data: [...dataArray.data, dtrInput] });
+      document.getElementById("cutoffInput").value = "";
+      document.getElementById("timeIn").value = "";
+      document.getElementById("breakStart").value = "";
+      document.getElementById("breakEnd").value = "";
+      document.getElementById("timeOut").value = "";
+      document.getElementById("bioId").value = "";
+      setDtrInput("");
+    }
+  };
+
+  console.log(dataArray);
+
+  const RemoveInput = (id) => {
+    // const newValue = dataArray.data.filter((fil) => fil.id != id);
+    // setDataArray({ ...dataArray, data: newValue });
+  };
+
+  return (
+    <div className="modal-container-main">
+      <div className="modal-container">
+        <h1 className="title">Hello</h1>
+        {dataArray.data.map((array) => {
+          return (
+            <>
+              <div className="form-content">
+                <div className="input-container">
+                  <label htmlFor="">Cutoff</label>
+                  <input
+                    type="text"
+                    placeholder="cutoff"
+                    defaultValue={array.cutoff}
+                    onChange={(e) =>
+                      setDtrInput({ ...dtrInput, cutoff: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="">Time in</label>
+                  <input
+                    type="text"
+                    placeholder="Time in"
+                    defaultValue={array.timeIn}
+                    onChange={(e) =>
+                      setDtrInput({ ...dtrInput, timeIn: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="">Break start</label>
+                  <input
+                    type="text"
+                    placeholder="Break start"
+                    defaultValue={array.breakStart}
+                    onChange={(e) =>
+                      setDtrInput({ ...dtrInput, breakStart: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="">Break end</label>
+                  <input
+                    type="text"
+                    placeholder="Break end"
+                    defaultValue={array.breakEnd}
+                    onChange={(e) =>
+                      setDtrInput({ ...dtrInput, breakEnd: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="">Time out</label>
+                  <input
+                    type="text"
+                    placeholder="Time out"
+                    defaultValue={array.timeOut}
+                    onChange={(e) =>
+                      setDtrInput({ ...dtrInput, timeOut: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="">Bio id</label>
+                  <input
+                    type="text"
+                    placeholder="Bio Id"
+                    defaultValue={array.bioId}
+                    onChange={(e) =>
+                      setDtrInput({ ...dtrInput, bioId: e.target.value })
+                    }
+                  />
+                </div>
+                {/* <button onClick={RemoveInput(array.id)}>Remove</button> */}
+              </div>
+            </>
+          );
+        })}
+        {dataArray.data.length == 10 ? (
+          ""
+        ) : (
+          <>
+            <div className="form-content">
+              <div className="input-container">
+                <label htmlFor="">Cutoff</label>
+                <input
+                  id="cutoffInput"
+                  type="text"
+                  placeholder="cutoff"
+                  onChange={(e) =>
+                    setDtrInput({
+                      ...dtrInput,
+                      id: uniqid(),
+                      cutoff: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Time in</label>
+                <input
+                  id="timeIn"
+                  type="text"
+                  placeholder="Time in"
+                  onChange={(e) =>
+                    setDtrInput({ ...dtrInput, timeIn: e.target.value })
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Break start</label>
+                <input
+                  id="breakStart"
+                  type="text"
+                  placeholder="Break start"
+                  onChange={(e) =>
+                    setDtrInput({ ...dtrInput, breakStart: e.target.value })
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Break end</label>
+                <input
+                  id="breakEnd"
+                  type="text"
+                  placeholder="Break end"
+                  onChange={(e) =>
+                    setDtrInput({ ...dtrInput, breakEnd: e.target.value })
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Time out</label>
+                <input
+                  id="timeOut"
+                  type="text"
+                  placeholder="Time out"
+                  onChange={(e) =>
+                    setDtrInput({ ...dtrInput, timeOut: e.target.value })
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Bio id</label>
+                <input
+                  id="bioId"
+                  type="text"
+                  placeholder="Bio Id"
+                  onChange={(e) =>
+                    setDtrInput({ ...dtrInput, bioId: e.target.value })
+                  }
+                />
+              </div>
+              <button className="addDtrData" onClick={SaveArray}>
+                +
+              </button>
+            </div>
+          </>
+        )}
+        <button className="submit-button">Save</button>
+      </div>
+    </div>
+  );
+};
+
+export default AddDtr;
+
+/*{
+  /**
+          * 
+          * 
+          * 
+          * 
+          * 
+          * 
+          * 
+        <form className="form-content">
+          <div className="input-container">
+            <label htmlFor="">Cutoff</label>
+            <input type="text" placeholder="cutoff" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="">Time in</label>
+            <input type="text" placeholder="Time in" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="">Break start</label>
+            <input type="text" placeholder="Break start" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="">Break end</label>
+            <input type="text" placeholder="Break end" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="">Time out</label>
+            <input type="text" placeholder="Time out" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="">Bio id</label>
+            <input type="text" placeholder="Bio Id" />
+          </div>
+        </form>
+          */
+//
--- frontend_testing_react_vite/client/src/pages/Timekeeping.jsx
+++ frontend_testing_react_vite/client/src/pages/Timekeeping.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import AddDtr from "../components/Timekeeping/addDtr";
 
 const Timekeeping = () => {
   const [dtr, setDtr] = useState([]);
+  const [openAdd, setOpenAdd] = useState(false);
   useEffect(() => {
     axios
       .get("http://localhost:9000/api/new_DTR/dtr_viewAll")
@@ -14,46 +16,51 @@ const Timekeeping = () => {
 
   console.log(dtr);
   return (
-    <section className="sectionContainer">
-      <div className="container">
-        <div className="content">
-          <h1 className="title">Timekeeping</h1>
-          <button className="add-dtr">Add Timerecord</button>
-          <table>
-            <thead>
-              <tr>
-                <th className="one">No.</th>
-                <th className="two">Cutoff</th>
-                <th className="three">Time in</th>
-                <th className="four">Break out</th>
-                <th className="five">Break in</th>
-                <th className="six">Time out</th>
-                <th className="seven">Bio ID</th>
-              </tr>
-            </thead>
-            <tbody>
-              <div className="td-content">
-                {dtr.map((data, index) => {
-                  return (
-                    <>
-                      <tr>
-                        <td className="td-one">{index + 1}</td>
-                        <td className="td-two">{data.Cutoff}</td>
-                        <td className="td-three">{data.Time_in}</td>
-                        <td className="td-four">{data.Time_break_out}</td>
-                        <td className="td-five">{data.Time_break_in}</td>
-                        <td className="td-six">{data.Time_out}</td>
-                        <td className="td-seven">{data.BioID}</td>
-                      </tr>
-                    </>
-                  );
-                })}
-              </div>
-            </tbody>
-          </table>
+    <>
+      <section className="sectionContainer">
+        <div className="container">
+          <div className="content">
+            <h1 className="title">Timekeeping</h1>
+            <button className="add-dtr" onClick={() => setOpenAdd(true)}>
+              Add Timerecord
+            </button>
+            <table>
+              <thead>
+                <tr>
+                  <th className="one">No.</th>
+                  <th className="two">Cutoff</th>
+                  <th className="three">Time in</th>
+                  <th className="four">Break out</th>
+                  <th className="five">Break in</th>
+                  <th className="six">Time out</th>
+                  <th className="seven">Bio ID</th>
+                </tr>
+              </thead>
+              <tbody>
+                <div className="td-content">
+                  {dtr.map((data, index) => {
+                    return (
+                      <>
+                        <tr>
+                          <td className="td-one">{index + 1}</td>
+                          <td className="td-two">{data.Cutoff}</td>
+                          <td className="td-three">{data.Time_in}</td>
+                          <td className="td-four">{data.Time_break_out}</td>
+                          <td className="td-five">{data.Time_break_in}</td>
+                          <td className="td-six">{data.Time_out}</td>
+                          <td className="td-seven">{data.BioID}</td>
+                        </tr>
+                      </>
+                    );
+                  })}
+                </div>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {openAdd && <AddDtr setOpenAdd={setOpenAdd} />}
+    </>
   );
 };
 
