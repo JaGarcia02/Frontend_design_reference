911d1bd63eaf166911a8878d0903ac10b6292d15
JaGarcia02 <jagarcia02051998@gmail.com>
2023-10-02 17:29:51 +0800

    sidebar new project

--- tailwind_references_components_pages/TailwindCss_References/Tailwind_Components_Pages/src/components/Sidebar/Sidebar.jsx
+++ tailwind_references_components_pages/TailwindCss_References/Tailwind_Components_Pages/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { SiIonic } from "react-icons/si";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const Sidebar = () => {
@@ -8,7 +9,7 @@ const Sidebar = () => {
       <div
         className={`${
           sideOpen ? "w-72" : "w-20"
-        }  h-screen bg-dark-purple relative transition-all duration-500 ease-in`}
+        } p-5 pt-8 h-screen bg-dark-purple relative transition-all duration-500 ease-in`}
       >
         {sideOpen === true && (
           <button
@@ -26,7 +27,23 @@ const Sidebar = () => {
             <AiOutlineArrowRight className=" text-[14px]" />
           </button>
         )}
+
+        <div className="flex gap-x-4 items-center ">
+          <div>
+            <SiIonic
+              className={`cursor-pointer duration-500 text-white text-[25px]`}
+            />
+          </div>
+          <h1
+            className={`text-white origin-left font-medium text-xl duration-300 ${
+              sideOpen === false && "scale-0"
+            }`}
+          >
+            Designer
+          </h1>
+        </div>
       </div>
+
       <div className="p-7 text-2xl font-semibold flex-1 h-screen">
         <h1>Home Page</h1>
       </div>
